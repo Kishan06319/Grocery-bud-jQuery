c4b2351b5f375e8917bdf3fecc41012b33acd790
Kishan06319 <kishankhatiwada44@gmail.com>
2026-02-05 20:48:05 +0545

    Added local jquery-4.0.0.min.js and fixed Day 3 rendering

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-  // Grocery items array
   let groceryItems = [
     { id: "1", name: "milk", completed: true },
     { id: "2", name: "bread", completed: true },
@@ -7,7 +6,6 @@ $(document).ready(function () {
     { id: "4", name: "butter", completed: false },
   ];
 
-  // Render items dynamically
   function renderItems(items) {
     const $app = $("#app");
     $app.empty();
@@ -21,7 +19,6 @@ $(document).ready(function () {
         .attr("type", "checkbox")
         .prop("checked", item.completed)
         .on("change", function () {
-          // Toggle completed state
           item.completed = $(this).is(":checked");
           renderItems(groceryItems);
         });
@@ -45,6 +42,5 @@ $(document).ready(function () {
     $app.append($container);
   }
 
-  // Initial render
   renderItems(groceryItems);
 });
